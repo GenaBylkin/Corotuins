04c4f60921c0e6f6501f13b74786807738d67efb
GenaBylkin <anton.nemanixin@mail.ru>
2023-08-17 10:03:29 +0300

    mainV

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,15 +2,62 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatCallback
+import androidx.core.view.isVisible
+import com.example.myapplication.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.buttonDownload.setOnClickListener {
+            loadData()
+        }
     }
 
 
 
+    private fun loadData() {
+        binding.progressBar.isVisible = true
+        binding.buttonDownload.isEnabled = false
+        loadCity{
+            binding.tvCity.text = it
+            loadTemperature(it) {
+                binding.tvTemperature.text = it.toString()
+                binding.progressBar.isVisible = false
+                binding.buttonDownload.isEnabled = true
+            }
+
+        }
+
+    }
+
+    private fun loadCity(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            callback.invoke("Moscow")
+        }
+
+    }
+
+    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
+        thread {
+            Toast.makeText(
+                this,
+                "Load temperature from $city",
+                Toast.LENGTH_LONG
+            ).show()
+            Thread.sleep(5000)
+            callback.invoke(17)
+        }
+    }
 
 
 }
